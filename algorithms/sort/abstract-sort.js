b1668e2c83a943a5d17c208a30bff27fc2080c26
suiqingxian <suiqingxian@gmail.com>
2017-12-20 22:02:46 +0800

    移除编译生成的js和js.map文件

--- algorithms/sort/abstract-sort.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AbstractSort = (function () {
-    function AbstractSort() {
-    }
-    return AbstractSort;
-}());
-exports.AbstractSort = AbstractSort;
-//# sourceMappingURL=abstract-sort.js.map
