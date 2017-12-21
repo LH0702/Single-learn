4c64d50cd57bffb7c20795d538f0ce38a19ba708
suiqingxian <suiqingxian@gmail.com>
2017-12-22 07:55:47 +0800

    最大子数组三种解法

--- algorithms/max-sub-array/max-sub-array.ts
+++ algorithms/max-sub-array/max-sub-array.ts
@@ -21,12 +21,49 @@ class MaxSubArray {
         return maxSubArray;
     }
 
-    public getMaxSubArrayByDivide(inputArray: number[]): number {
+    public getMaxSubArrayByDivide = (inputArray: number[]): number  => {
         if (inputArray.length === 0) {
             throw new Error('invalid input');
         }
+        
+        return this.getMaxSubArray(inputArray,0,inputArray.length-1);
+    }
 
-        return;
+    private getMaxSubArray(inputArray:number[],leftIndex:number,rightIndex:number):number{
+        if(leftIndex >= rightIndex){
+            return inputArray[leftIndex];
+        }
+
+        let midIndex = Math.floor((leftIndex + rightIndex)/2);
+        let left = this.getMaxSubArray(inputArray,leftIndex,midIndex);
+        let right = this.getMaxSubArray(inputArray,midIndex + 1,rightIndex);
+        let mid = this.getCrossMaxSunArray(inputArray,leftIndex,rightIndex,midIndex);
+
+        return Math.max(Math.max(left,right),mid);
+
+    }
+
+    private getCrossMaxSunArray(inputArray:number[],leftIndex:number,rightIndex:number,midIndex:number):number{
+
+        let leftMaxSum = Number.NEGATIVE_INFINITY;
+        let leftSum:number = 0;
+        for(let i = midIndex; i>= leftIndex; i--){
+            leftSum += inputArray[i];
+            if(leftMaxSum < leftSum){
+                leftMaxSum = leftSum; 
+            }
+        }
+
+        let rightMaxSum = Number.NEGATIVE_INFINITY;
+        let rightSum:number = 0;
+        for(let i = midIndex+1; i <= rightIndex ;i++){
+            rightSum += inputArray[i];
+            if(rightMaxSum < rightSum){
+                rightMaxSum = rightSum; 
+            }
+        }
+
+        return leftMaxSum + rightMaxSum;
     }
 
     public getMaxSubArrayByLinear(inputArray: number[]): number {
