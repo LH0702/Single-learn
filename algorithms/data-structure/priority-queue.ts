1a7dbedbc011c526bcdf4088290585feadd47bcf
suiqingxian <suiqingxian@gmail.com>
2018-05-05 22:23:03 +0800

    priority queue test case

--- algorithms/data-structure/priority-queue.ts
+++ algorithms/data-structure/priority-queue.ts
@@ -1,45 +1,98 @@
 /**
  * minimum
  */
-export class PriorityQueue{
-    private queue : number[] = [];
-    
-    insert(input: number| number[]):PriorityQueue{
-        if(input instanceof Number){
-           this.queue.push(<number>input) 
-           this.changePriority(this.queue.length - 1);
-        }else if (input instanceof Array){
-            for(let e of input){
+export class PriorityQueue {
+    private queue: number[] = [];
+
+    insert(input: number | number[]): PriorityQueue {
+        if (typeof input === 'number') {
+            this.queue.push(Number.MAX_SAFE_INTEGER);
+            this.increaseKey(this.queue.length - 1, <number>input);
+        } else if (input instanceof Array) {
+            for (let e of input) {
                 this.queue.push(e);
-                this.changePriority(this.queue.length - 1);
+                this.increaseKey(this.queue.length - 1, e);
             }
-        }else{
+        } else {
             throw new Error("Input type is invalid");
         }
         return this
     }
 
-    minimum():number{
-        if(this.queue.length === 0){
+    minimum(): number {
+        if (this.queue.length === 0) {
             return null;
         }
 
         return this.queue[0];
     }
 
-    extractMin():number{
-        return;
+    extractMin(): number {
+
+        if (this.queue.length === 0) {
+            return null;
+        }
+
+        let min = this.queue[0];
+        this.queue[0] = this.queue.pop();
+        this.minHeapify(0);
+        return min;
     }
 
-    changePriority(index:number,key ?:number ){
+    increaseKey(index: number, key: number) {
+        if (key > this.queue[index]) {
+            throw new Error('input key is larger than current');
+        }
+
+        this.queue[index] = key;
+        let tmpIdx = index;
+        while (tmpIdx > 0 && this.queue[this.getParent(tmpIdx)] > key) {
+            this.swap(tmpIdx,this.getParent(tmpIdx));
+            tmpIdx = this.getParent(tmpIdx);
+        }
 
     }
 
-    clear(){
+    clear() {
         this.queue = [];
     }
 
-    private minHeapify(index:number){
+    length() {
+        return this.queue.length;
+    }
 
+    private minHeapify(idx: number) {
+        let minIndex = idx;
+        let length = this.queue.length;
+        if (this.getLeft(idx) < length && this.queue[minIndex] > this.queue[this.getLeft(idx)]) {
+            minIndex = this.getLeft(idx);
+        }
+
+        if (this.getRight(idx) < length && this.queue[minIndex] > this.queue[this.getRight(idx)]) {
+            minIndex = this.getRight(idx);
+        }
+
+        if (minIndex !== idx) {
+            this.swap(idx,minIndex);
+            this.minHeapify(minIndex);
+        }
+    }
+
+    private swap(idx1: number, idx2: number) {
+        let tmp = this.queue[idx1];
+        this.queue[idx1] = this.queue[idx2];
+        this.queue[idx2] = tmp;
+    }
+
+    private getLeft(index: number): number {
+        return (index + 1) * 2 - 1;
+    }
+
+    private getRight(index: number): number {
+        return (index + 1) * 2;
+    }
+
+    private getParent(index: number): number {
+        return Math.floor((index - 1) / 2);
     }
 }
--- algorithms/test/priority-queue/priority-queue-test.ts
+++ algorithms/test/priority-queue/priority-queue-test.ts
@@ -6,25 +6,25 @@ describe('优先队列测试', () => {
         it('add and get test', () => {
             let pq : PriorityQueue  = new PriorityQueue();
             pq.insert(3).insert(5).insert(9).insert([2,4,5,4,7,32,12,15,55]);
-            expect(2).to.be.equal(pq.get());
-            expect(3).to.be.equal(pq.get());
-            expect(4).to.be.equal(pq.get());
-            expect(4).to.be.equal(pq.get());
-            expect(5).to.be.equal(pq.get());
-            expect(5).to.be.equal(pq.get());
-            expect(7).to.be.equal(pq.get());
-            expect(9).to.be.equal(pq.get());
-            expect(12).to.be.equal(pq.get());
-            expect(15).to.be.equal(pq.get());
-            expect(32).to.be.equal(pq.get());
-            expect(55).to.be.equal(pq.get());
+            expect(2).to.be.equal(pq.extractMin());
+            expect(3).to.be.equal(pq.extractMin());
+            expect(4).to.be.equal(pq.extractMin());
+            expect(4).to.be.equal(pq.extractMin());
+            expect(5).to.be.equal(pq.extractMin());
+            expect(5).to.be.equal(pq.extractMin());
+            expect(7).to.be.equal(pq.extractMin());
+            expect(9).to.be.equal(pq.extractMin());
+            expect(12).to.be.equal(pq.extractMin());
+            expect(15).to.be.equal(pq.extractMin());
+            expect(32).to.be.equal(pq.extractMin());
+            expect(55).to.be.equal(pq.extractMin());
         });
 
         it('clear 测试', () => {
             let pq : PriorityQueue  = new PriorityQueue();
             pq.insert(3).insert(5).insert(9).insert([2,4,5,4,7,32,12,15,55]);
             pq.clear();
-            expect(null).to.be.equal(pq.get());
+            expect(null).to.be.equal(pq.extractMin());
         });
 
 })
